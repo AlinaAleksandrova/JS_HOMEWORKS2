51def9ce941a46e9737c2fc1c7e9bd12f8c47894
Alina <aleksandrovaalina4@gmail.com>
2024-02-29 22:07:37 +0200

    change const

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 'use strict';
-
-let message = prompt("How old are you?");
+const message = prompt("How old are you?");
 const age = Number(message);
 const futureAge = age + 10;
 alert("In 10 years you will be" + " " + futureAge + " " + "years old.");
