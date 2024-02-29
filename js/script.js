cf2efa12e7435d78f1d72b7f9317bdd87399e887
Alina <aleksandrovaalina4@gmail.com>
2024-02-29 13:22:49 +0200

    celsius/fahrenheit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const degree = prompt("Enter value:");
+const unit= prompt("Enter unit of measurement: (C or F)");
+console.log(degree + " " + unit);
+const celsius =(Number(degree) - 32) / 1.8;
+const fahrenheit = Number(degree) * 1.8 + 32;
+
+if (unit === "F" || unit === "f"){
+    console.log(celsius + " " + "°C");
+}
+else if(unit === "C" || unit === "c" ){
+    console.log(fahrenheit + " " + "F");
+}
+else
+    console.log("ERROR: You entered an incorrect letter")
