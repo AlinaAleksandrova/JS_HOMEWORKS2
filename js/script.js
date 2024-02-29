edd7143aad08427c0eedd11531e665a335b43632
Alina <aleksandrovaalina4@gmail.com>
2024-02-29 22:30:05 +0200

    Amazing calculator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const a = prompt("Enter the first value:");
+const b = prompt("Enter the second value:");
+
+const addition = Number(a) + Number(b);
+const subtraction = Number(a) - Number(b);
+const multiplication = Number(a) * Number(b);
+const division = Number(a) / Number(b);
+
+console.log("Rezalt:" + " " + a + " + " + b + " " + "=" + " " + addition );
+console.log("Rezalt:" + " " + a + " - " + b + " " + "=" + " " + subtraction );
+console.log("Rezalt:" + " " + a + " * " + b + " " + "=" + " " + multiplication );
+console.log("Rezalt:" + " " + a + " / " + b + " " + "=" + " " + division );
