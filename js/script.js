65df82d8e60b23c533e8af9e269434612c0c6b35
Alina <aleksandrovaalina4@gmail.com>
2024-03-05 18:01:08 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const value1 = +prompt("Enter first value:");
+const value2 = +prompt("Enter second value:");
+const value3 = +prompt("Enter third value:");
+const arithmetic = (value1 + value2 + value3) / 3;
+
+if(isNaN(value1)){
+    alert("You entered an incorrect number! Try your luck again!");
+}
+else{
+    alert("The arithmetic mean of these numbers: " + value1 + " + " + value2 + " + " + value3 + " = " + " " + arithmetic);
+}
