5036421b9e96101208886f1942b8f9dd16bbe59a
Alina <aleksandrovaalina4@gmail.com>
2024-03-05 17:46:09 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const value1 = prompt("Enter first value:");
+const value2 = prompt("Enter second value:");
+const sum = Number(value1) + Number(value2);
+const sub = Number(value1) - Number(value2);
+const mult = Number(value1) * Number(value2);
+const div = Number(value1) / Number(value2);
+
+if (isNaN(value1)){
+    alert("You entered an incorrect number! Try your luck again!");
+}
+else{
+    alert(`The user entered ${value1} and ${value2}: 
+    \n ${value1} + ${value2} = ${sum} 
+    \n ${value1} - ${value2} = ${sub}
+    \n ${value1} * ${value2} = ${mult}
+    \n ${value1} / ${value2} = ${div}
+    `);
+}
