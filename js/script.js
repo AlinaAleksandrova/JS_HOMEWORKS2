ac3d9b1830628dabaad29d9fb6a17a2658b79fcf
Alina <aleksandrovaalina4@gmail.com>
2024-03-08 18:04:34 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const question = prompt("What operation do you want to perform: add, sub, mult, div");
+const questionLowerCase = question.toLowerCase();
+const value1 = +prompt("Enter first value:");
+const value2 = +prompt("Enter second value:");
+const add = value1 + value2;
+const sub = value1 - value2;
+const mult = value1 * value2;
+const div = value1 / value2;
+
+
+if (isNaN(value1)){
+    alert("It's not a number :( Try again.")
+}
+else {
+    if(question === "ADD" || question === "Add" || question === "add" ){
+        alert("Result: " + value1 + " + " + value2 + " = " + add );
+    }
+    else if(question === "SUB" || question === "Sub" || question === "sub" ){
+        alert("Result: " + value1 + " - " + value2 + " = " + sub );
+    }
+    else if(question === "MULT" || question === "Mult" || question === "mult" ){
+        alert("Result: " + value1 + " * " + value2 + " = " + mult );
+    }
+    else if(question === "DIV" || question === "Div" || question === "div" ){
+        alert("Result: " + value1 + " / " + value2 + " = " + div );
+    }
+    else {
+        alert("Enter the correct action")
+    }
+}
