ca813c9c50609e3d31080f62227d7f8faeb76f21
Alina <aleksandrovaalina4@gmail.com>
2024-03-22 18:17:26 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const number = +prompt("Введіть будь-ласка число: ");
+
+if(number < 1 || isNaN(number)){
+    console.log(NaN)
+}else{
+    let divider = NaN;
+
+    for(let i = 2; i < number; i++){
+        if(number % i === 0){
+            divider = i;
+            break;
+        }else if(i === number){
+            console.log("Число просте");
+        }
+    }
+    if (isNaN(divider)) {
+        console.log("Число " + number + " є простим");
+    } else {
+        console.log("Мінімальний дільник числа " + number + " - це число " + divider);
+    }
+}
