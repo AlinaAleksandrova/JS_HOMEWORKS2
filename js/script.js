d976119cbcd8474afce5e5d6a08786501bd71164
Alina <aleksandrovaalina4@gmail.com>
2024-03-22 19:26:30 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let sum = 0
+outerLoop:for(let i = 0; i <= 20; i++){
+    if(i % 2 === 0){
+        continue;
+    }
+    sum = sum + i;
+    if (i === 19) {
+        break outerLoop;
+    }
+}
+console.log("Сумма непарних чисел = " + sum);
