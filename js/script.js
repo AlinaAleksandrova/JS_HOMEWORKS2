02d2cf3b2ab68eef8b7754ebc57c3354f6016e37
Alina <aleksandrovaalina4@gmail.com>
2024-03-23 17:30:51 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const charactersFunction = function generateKey(length, characters){
+    let res = "";
+    const charLength = characters.length;
+    for(let i = 0; i <= length; i++){
+        const index = i % charLength;
+        res = res + characters[index];
+    }
+    return res;
+}
+
+console.log(charactersFunction(16, characters));
