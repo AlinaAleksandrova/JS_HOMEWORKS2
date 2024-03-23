5baf22010bd529b0cc2ca7f4dea343346fdf3ff1
Alina <aleksandrovaalina4@gmail.com>
2024-03-23 17:35:04 +0200

    add Math.floor and random

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const charactersFunction = function generateKey(length, characters){
     let res = "";
     const charLength = characters.length;
     for(let i = 0; i <= length; i++){
-        const index = i % charLength;
+        const index = Math.floor(Math.random() * charLength);
         res = res + characters[index];
     }
     return res;
