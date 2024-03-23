695ebad19da4f8e9350cdacd6599089cbc726ad7
Alina <aleksandrovaalina4@gmail.com>
2024-03-23 17:59:32 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const number = +prompt("Введіть будь-ласка число яка хочете хочете піднести в ступінь: ");
+const degree = +prompt("Введіть будь-ласка СТУПІНЬ в яку хочете піднести число: ");
+const advancementInDegree = function isDegree(number, degree = 1){
+    const degreeMath = number ** degree;
+    if (isNaN(number) && isNaN(degree) || isNaN(number) || isNaN(degree)){
+        return "Ви ввели не числа. Спробуйте ще раз!";
+    }
+    else{
+        return degreeMath;
+    }
+}
+const res = advancementInDegree(number, degree);
+alert("Якщо число " + number + " піднести в ступінь " + degree + " то результат буде = " + res);
