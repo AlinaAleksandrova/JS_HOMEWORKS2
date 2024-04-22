3d746c6895ef6a7c335df1a75820670d08967a1c
Alina <aleksandrovaalina4@gmail.com>
2024-04-22 16:59:01 -0500

    bind function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+
+const bind = function(context) {
+    const variable = this;
+    const arr = [];
+    for (let i = 1; i < arguments.length; i++) {
+        arr.push(arguments[i]);
+    }
+    return function() {
+        const variable2 = [];
+        for (let i = 0; i < arguments.length; i++) {
+            variable2.push(arguments[i]);
+        }
+        const allArr = args.concat(funcArgs);
+        const res = variable(...allArgs);
+        return res;
+    };
+};
+
+console.log(bind)
+
+
+
+
