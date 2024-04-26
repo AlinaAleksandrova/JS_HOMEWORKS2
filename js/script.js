cc13da6afc5efbb225e9695a4f3474d83abcee9b
Alina <aleksandrovaalina4@gmail.com>
2024-04-26 13:34:09 -0500

    Closures func

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+(function () {
+    const funClosures = () => {
+        let sum = 0;
+        return (num) => sum += num;
+    };
+
+    const sum = funClosures();
+
+    console.log(sum(3));
+    console.log(sum(5));
+    console.log(sum(20));
+})();
