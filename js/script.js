694bff353ad05ef53c2fb93e651e3c41dcb55c58
Alina <aleksandrovaalina4@gmail.com>
2024-05-08 23:21:21 -0500

    DOM 3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const generateList = (array) => {
+    const ul = document.createElement('ul');
+    for(let item of array){
+
+        const li = document.createElement('li');
+
+        if(Array.isArray(item)){
+            li.appendChild(generateList(item));
+        }else{
+            li.textContent = item
+        }
+        ul.appendChild(li)
+    }
+    return ul;
+}
+
+const arr = [1,2, [1.1,1.2,1.3] ,3];
+const list = generateList(arr);
+document.body.appendChild(list);
