b7226a4ed3ee53b3e19bcccfe0aed1835a4d4931
Alina <aleksandrovaalina4@gmail.com>
2024-06-25 11:27:51 -0500

    add script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+(function (){
+    const arr = [1,2,3,4,5,6,7,8,9,0];
+    const itr = arr[Symbol.iterator]();
+
+    const createArrItr = function (arr){
+        let i = 0;
+        return{
+            next: function (){
+                if(i < arr.length){
+                    return{value:arr[i++], done: false}
+                }
+                else{
+                    return {value: undefined, done: true}
+                }
+            }
+        }
+    }
+
+    Array.prototype[Symbol.iterator] = function (){
+        return createArrItr(this);
+    }
+
+    console.log(itr.next());
+    console.log(itr.next());
+    console.log(itr.next());
+    console.log(itr.next());
+    console.log(itr.next());
+    console.log(itr.next());
+    console.log(itr.next());
+    console.log(itr.next());
+    console.log(itr.next());
+    console.log(itr.next());
+    console.log(itr.next());
+
+})()
+
