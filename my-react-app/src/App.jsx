3ac93afc8ee5b70151641368fe19b4125e67b4ef
Alina <aleksandrovaalina4@gmail.com>
2024-07-29 12:41:51 -0500

    component card

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import React, { Component } from 'react';
+import Card from './components/Card.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-      <></>
-  )
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <div className="container mt-5">
+            <Card />
+          </div>
+        </div>
+    );
+  }
 }
 
-export default App
+export default App;
