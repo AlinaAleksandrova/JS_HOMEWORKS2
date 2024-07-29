941ea8b8cac004a8110ded2cfa71ec1005822b19
Alina <aleksandrovaalina4@gmail.com>
2024-07-29 13:26:07 -0500

    app.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from 'react'
+import React, { Component } from 'react';
+import Card from './components/Card.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <></>
-  )
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="container mt-5">
+                    <Card title="Hi" />
+                    <Card text="How are you?" />
+                    <Card title="Hi" text="How are you?" />
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App
+export default App;
