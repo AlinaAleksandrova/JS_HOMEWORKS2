5655a1fa70b7b76a80f133b6f148edc54d1d92e4
Alina <aleksandrovaalina4@gmail.com>
2024-07-29 17:11:34 -0500

    app.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,23 @@
-import { useState } from 'react'
+import React, {Component} from "react";
+import Definitions from './components/Definitions.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <></>
-  )
+class App extends Component{
+  render() {
+
+      const definitions = [
+        { dt: 'one', dd: 'two', id: 1 },
+        { dt: 'another term', dd: 'another description', id: 2 },
+      ];
+
+      return (
+          <div className="App">
+              <div className="container mt-5">
+                  <Definitions data={definitions} />
+              </div>
+          </div>
+      );
+  }
 }
 
-export default App
+export default App;
