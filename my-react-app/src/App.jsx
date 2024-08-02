f29ae2235f6f658091dadd89a1dfe13556a66f0f
Alina <aleksandrovaalina4@gmail.com>
2024-08-01 23:18:15 -0500

    bootstrap components

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 import ImageSlider from './components/ImageSlider.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
     constructor(props) {
