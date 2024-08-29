419a58ebab923e51f21c2eacbb2dddf062ac7d12
Alina <aleksandrovaalina4@gmail.com>
2024-08-29 18:38:36 -0500

    add nav

--- contacts-manager/src/App.js
+++ contacts-manager/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import ContactForm from './components/ContactForm';
@@ -9,9 +9,15 @@ function App() {
     return (
         <Provider store={store}>
             <Router>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/add">Add New Contact</Link></li>
+                    </ul>
+                </nav>
                 <Routes>
-                    <Route path="/" element={<ContactList />} />
-                    <Route path="/add" element={<ContactForm />} />
+                    <Route path="/" element={<ContactList/>}/>
+                    <Route path="/add" element={<ContactForm/>}/>
                 </Routes>
             </Router>
         </Provider>
