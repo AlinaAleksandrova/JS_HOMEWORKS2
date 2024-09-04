c4a568c3a3dcfd89436f0c4b99c3e084dfe3b72e
Alina <aleksandrovaalina4@gmail.com>
2024-09-04 17:53:27 -0500

    mvc structure

---
+++ scripts/TodoList/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import Model from './Model.js';
+import View from './View.js';
+import Controller from './Controller.js';
+
+const selectors = {
+    form: '[data-form]',
+    todosContainer: '#todoForm',
+    clearButton: '[data-clear-btn]',
+};
+
+View.init(selectors);
+Model.init();
+Controller.init(selectors);
